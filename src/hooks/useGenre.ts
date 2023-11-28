2025a62ffc54bf0c3e1c5a61a825c55ad3809df7
Basty <bastygottocollege@gmail.com>
2023-11-29 00:22:26 +0800

    fetch data with react query

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,7 @@
 import genres from '../data/genres'
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../services/api-client';
+import { FetchResponse } from './useData';
 
 export interface Genre{
     id: number;
@@ -6,6 +9,15 @@ export interface Genre{
     image_background: string;
 }
 
-const useGenre = () => ({data: genres, isLoading: false, error: null});
+const getAll = () => ({data: genres, isLoading: false, error: null});
+
+const useGenre = () => useQuery({
+    queryKey: ["genres"],
+    queryFn: () => apiClient
+        .get<FetchResponse<Genre>>("/genres")
+        .then(res => res.data),
+    staleTime: 60 * 1000,
+    initialData: {count: genres.length, results: genres}
+});
 
 export default useGenre; 
